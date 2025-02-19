3086ca384a3dd189fce925b1a60773332f262b52
احمد 31156952 <UG_31156952@f-eng.tanta.edu.eg>
2025-02-19 13:05:51 +0200

    Finished

---
+++ MyImageService/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyImageService.Interfaces;
+using MyImageService.Service;
+
+namespace MyImageService
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterImageServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUploadFileService, UploadFileService>();
+        }
+    }
+}
